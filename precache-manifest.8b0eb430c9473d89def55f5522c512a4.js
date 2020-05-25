2294851fee75edb1a51cfaa3d3ba627505d197ea
deanzaka <hdeanzak@id.ibm.com>
2020-05-25 10:57:40 +0700

    Updates

---
+++ precache-manifest.8b0eb430c9473d89def55f5522c512a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b685af9957ba34c80781d9daf265996",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "5ffd80bae784313a5ad5",
+    "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4f591072e70d767e8911",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5ffd80bae784313a5ad5",
+    "url": "/contact-manager/static/js/2.846bde5a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.846bde5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f591072e70d767e8911",
+    "url": "/contact-manager/static/js/main.637cca28.chunk.js"
+  },
+  {
+    "revision": "137103d1533e344059f4",
+    "url": "/contact-manager/static/js/runtime-main.0cf1163a.js"
+  }
+]);
