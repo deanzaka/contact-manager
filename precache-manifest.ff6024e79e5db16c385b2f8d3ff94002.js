712d33c512092dd7c41c907e01238755d9d92c27
deanzaka <hdeanzak@id.ibm.com>
2020-05-25 11:00:44 +0700

    Updates

--- precache-manifest.ff6024e79e5db16c385b2f8d3ff94002.js
+++ precache-manifest.ff6024e79e5db16c385b2f8d3ff94002.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7b685af9957ba34c80781d9daf265996",
+    "revision": "2ec2482cf8ce7acc1dbbd07053e91786",
     "url": "/contact-manager/index.html"
   },
   {
-    "revision": "5ffd80bae784313a5ad5",
+    "revision": "328849f7d54dcf6bb7fd",
     "url": "/contact-manager/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "4f591072e70d767e8911",
+    "revision": "f48f7673ee33c0bef143",
     "url": "/contact-manager/static/css/main.36497136.chunk.css"
   },
   {
-    "revision": "5ffd80bae784313a5ad5",
-    "url": "/contact-manager/static/js/2.846bde5a.chunk.js"
+    "revision": "328849f7d54dcf6bb7fd",
+    "url": "/contact-manager/static/js/2.0cf93dc0.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/contact-manager/static/js/2.846bde5a.chunk.js.LICENSE.txt"
+    "url": "/contact-manager/static/js/2.0cf93dc0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f591072e70d767e8911",
-    "url": "/contact-manager/static/js/main.637cca28.chunk.js"
+    "revision": "f48f7673ee33c0bef143",
+    "url": "/contact-manager/static/js/main.3025d977.chunk.js"
   },
   {
     "revision": "137103d1533e344059f4",
